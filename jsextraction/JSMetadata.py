940bafe8fd1192bce57e0b10b8208abc20f2be94
Willi Lazarov <38925292+larescze@users.noreply.github.com>
2022-10-29 22:32:09 +0200

    Revert "2022-10-27_RK_Detect-presence-of-JavaScript"

--- jsextraction/JSMetadata.py
+++
@@ -1,5 +0,0 @@
-class JSMetadata:
-    def __init__(self, external_js: int, local_js: int, inline_js: int) -> None:
-        self.external_js = external_js
-        self.local_js = local_js
-        self.inline_js = inline_js
--- ptwebcategory.py
+++ ptwebcategory.py
@@ -22,8 +22,7 @@ class ptwebcategory:
             csv_provider = CsvProvider(self.args.file)
             #...
             csv_provider.extract_forms()
-            csv_provider.extract_javascript()
-            #csv_provider.extract_css()
+            #csv_provider.extract_javascript()
             #...
             csv_provider.save_file()
         sys.exit(0)
