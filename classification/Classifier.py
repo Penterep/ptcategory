48f8d99e2d408951306e3f3936f14035dd1f9300
Michal Kaiser <xkaiser.m@gmail.com>
2022-11-18 19:01:51 -0500

    edit K-means mini batch graph title

--- classification/Classifier.py
+++ classification/Classifier.py
@@ -44,7 +44,7 @@ class Classifier:
     def kmeans_mini_batch(self, init: str, max_iter: int, init_clusters: int, batch_size: int) -> None:
         model = MiniBatchKMeans(init=init, max_iter=max_iter, n_init=init_clusters, batch_size=batch_size)
         df = self._get_clustered_dataframe(model)
-        self._display_parallel_coordinates(df, "K-means")
+        self._display_parallel_coordinates(df, "K-means mini batch")
 
     def _get_clustered_dataframe(self, model) -> pd.DataFrame:
         dataset_copy = self.dataset.get_copy()
