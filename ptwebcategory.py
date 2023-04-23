52c01e34938187bcdbaebf4f5f5a5d2afaf00d39
Dominik Žáček <dominik.zacek@pinya.cz>
2023-04-23 15:38:58 +0200

    clustering method selection throught args

--- ptwebcategory.py
+++ ptwebcategory.py
@@ -5,6 +5,7 @@ __version__ = "0.0.1"
 import argparse
 import sys
 import logging
+from typing import Callable
 
 from halo import Halo
 
@@ -20,6 +21,19 @@ from exporting.JsonExporter import JsonExporter
 class ptwebcategory:
     SHOW_EXPORT_PARAMETERS_ARG = "parameters"
     SHOW_EXPORT_AVERAGES_ARG = "averages"
+    methods: dict[str, Callable[[Clustering], Callable]] = {
+                'mean_shift': lambda classifier: classifier.mean_shift,
+                'optics': lambda classifier: classifier.optics,
+                'spectral_clustering': lambda classifier: classifier.spectral_clustering,
+                'gaussian_mixture': lambda classifier: classifier.gaussian_mixture,
+                'dbscan': lambda classifier: classifier.dbscan,
+                'birch': lambda classifier: classifier.birch,
+                'affinity_propagation': lambda classifier: classifier.affinity_propagation,
+                'agglomerative_clustering': lambda classifier: classifier.agglomerative_clustering,
+                'kmeans': lambda classifier: classifier.kmeans,
+                'kmeans_mini_batch': lambda classifier: classifier.kmeans_mini_batch,
+                'manual_clustering': lambda classifier: classifier.manual_clustering
+            }
     
     def __init__(self, args):
         self.ptjsonlib = ptjsonlib.ptjsonlib(args.json)
@@ -49,30 +63,11 @@ class ptwebcategory:
                 csv_provider.save_file()
             dataset = Dataset(csv_provider.rows_dict, start_from_col=2)
             classifier = Clustering(dataset)
-            logging.info("Mean shift clustering")
-            classified_df = classifier.mean_shift()
-            logging.info("Optics clustering")
-            classifier.optics()
-            logging.info("Spectral clustering")
-            classifier.spectral_clustering()
-            logging.info("Gaussian mixture clustering")
-            classifier.gaussian_mixture()
-            logging.info("DBSCAN clustering")
-            classifier.dbscan()
-            logging.info("Birch clustering")
-            classifier.birch()
-            logging.info("Affinity propagation clustering")
-            classifier.affinity_propagation()
-            logging.info("Agglomerative clustering")
-            classifier.agglomerative_clustering()
-            logging.info("K-means clustering")
-            classifier.kmeans()
-            logging.info("K-means mini batch clustering")
-            classifier.kmeans_mini_batch()
-            logging.info("Manual clustering")
-            classifier.manual_clustering()
-            logging.info(f"Manual clustering... Done")
-            
+            clustering_method_name = self.args.clustering_method
+            clustrering_method = self.methods[clustering_method_name](classifier)
+            logging.info(f"{clustering_method_name} clustering")
+            classified_df = clustrering_method()
+            logging.info(f"{clustering_method_name} clustering... Done")
             if self.args.json is not None:
                 exporter = JsonExporter(csv_provider.get_urls())
                 exporter.export_with_parameters = self.SHOW_EXPORT_PARAMETERS_ARG in self.args.json
@@ -82,7 +77,7 @@ class ptwebcategory:
             
             input("Press enter to exit...")
         sys.exit(0)
-
+        
 
 def get_help():
     return [
@@ -94,10 +89,11 @@ def get_help():
         ]},
         {"options": [
             ["-f", "--file", "<file path>", "Load urls from file"],
-            ["-j", "--json", "", "Output in JSON format"],
+            ["-j", "--json", "", "Output in JSON format. Available options: parameters, averages"],
             ["-v", "--version", "", "Show script version and exit"],
             ["-h", "--help", "", "Show this help message and exit"],
-            ["-e", "--evaluation-only", "", "Run only evaluation on already created dataset"]
+            ["-e", "--evaluation-only", "", "Run only evaluation on already created dataset"],
+            ["-m", "--clustering-method", "", "Clustering method to use. Default: kmeans. Available: mean_shift, optics, spectral_clustering, gaussian_mixture, dbscan, birch, affinity_propagation, agglomerative_clustering, kmeans, kmeans_mini_batch, manual_clustering"]
         ]
         }]
 
@@ -110,6 +106,7 @@ def parse_args():
     parser.add_argument("-v", "--version", action="version",
                         version=f"%(prog)s {__version__}")
     parser.add_argument("-e", "--evaluation-only", action="store_true")
+    parser.add_argument("-m", "--clustering-method", choices=list(ptwebcategory.methods.keys()), default="kmeans")
 
     if len(sys.argv) == 1 or "-h" in sys.argv or "--help" in sys.argv:
         ptmisclib.help_print(get_help(), SCRIPTNAME, __version__)
